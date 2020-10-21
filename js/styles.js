e1b23a429ad6937a6fdc573e86e4acfa1dd1a61b
Patrick Osten <Patrickosten040@gmail.com>
2020-10-21 15:15:02 -0700

    create directories for project
    co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ js/styles.js
@@ -0,0 +1,7 @@
+$(document.ready(function() {
+  function add(number1, number2) {
+    return number1 + number2;
+  }
+});
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
